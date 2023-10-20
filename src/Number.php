ca14b75c7c69c7f4e8cfd8f62e4ed6215e9d10df
macocci7 <macocci7@yahoo.co.jp>
2023-10-20 15:54:00 +0900

    #1: create base functions - src/Number.php updated - tests/NumberTest.php updated

--- src/Number.php
+++ src/Number.php
@@ -70,15 +70,80 @@ class Number
         return (bool) $r;
     }
 
-    public function isPositive($n)
+    public function isFraction($n)
     {
+        if (!$this->isFloat($n)) {
+            return false;
+        }
+        return abs($n) > 0 && abs($n) < 1;
     }
 
-    public function getNthDigit($n, $d)
+    public function getSign($n)
     {
+        if (!$this->isNumber($n)) {
+            return;
+        }
+        return $n > 0 ? 1 : ($n < 0 ? -1 : 0);
+    }
+
+    public function getInt($n)
+    {
+        if (!$this->isNumber($n)) {
+            return;
+        }
+        return (int) $n;
+    }
+
+    public function getFraction($n)
+    {
+        if (!$this->isNumber($n)) {
+            return;
+        }
+        return (float) ($n - $this->getInt($n));
+    }
+
+    public function getNthDigit(int $n, $d)
+    {
+        if (!$this->isNumber($d)) {
+            return;
+        }
+        if ($n > 0) {
+            // integer part
+            $s = (string) $this->getInt(abs($d));
+            if ($n > strlen($s)) {
+                return;
+            }
+            return (int) substr($s, -$n, 1);
+        } elseif ($n < 0) {
+            // fraction part
+            $s = (string) $this->getFraction(abs($d));
+            if (-$n > strlen($s) - 2) {
+                return;
+            }
+            return (int) substr($s, 1 - $n, 1);
+        }
+        return;
     }
 
     public function getNumberOfDigits($n)
     {
+        if (!$this->isNumber($n)) {
+            return;
+        }
+        return strlen((string) abs($this->getInt($n)));
+    }
+
+    public function getNumberOfFractionalDigits($n)
+    {
+        if (!$this->isNumber($n)) {
+            return;
+        }
+        if ($this->isInt($n)) {
+            return 0;
+        }
+        if (strcmp("0", (string) $this->getFraction($n)) === 0) {
+            return 0;
+        }
+        return strlen((string) $this->getFraction(abs($n))) - 2;
     }
 }
