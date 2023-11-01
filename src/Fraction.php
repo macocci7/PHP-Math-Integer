6ef76fdf40c616a8f774f2dbc8215a36ec9bdb7b
macocci7 <macocci7@yahoo.co.jp>
2023-11-01 17:31:09 +0900

    #1: create base funcitons

--- src/Fraction.php
+++ src/Fraction.php
@@ -3,38 +3,69 @@
 namespace Macocci7\PhpMathInteger;
 
 use Macocci7\PhpMathInteger\Divisor;
+use Macocci7\PhpMathInteger\Euclid;
 
 class Fraction extends Divisor
 {
+    public $wholeNumbers;
+
     public $numerator;
 
     public $denominator;
+
+    private $e; // Macocci7\PhpMathInteger\Euclid
 
     /**
      * constructor
      */
     public function __construct()
     {
+        $this->e = new Euclid();
     }
 
     public function isReduced()
     {
+        return 1 === $this->numerator && 1 === $this->denominator
+               ? true
+               : $this->e->isCoprime($this->numerator, $this->denominator)
+               ;
     }
 
     public function isProper()
     {
+        return !is_null($this->wholeNumbers)
+               ? false
+               : (
+                    $this->numerator > 0 && $this->denominator > 0
+                    ? $this->numerator < $this->denominator
+                    : false
+                 )
+               ;
     }
 
     public function isImproper()
     {
+        return !is_null($this->wholeNumbers)
+               ? false
+               : (
+                    $this->numerator > 0 && $this->denominator > 0
+                    ? $this->numerator > $this->denominator
+                    : false
+                 )
+               ;
     }
 
     public function isMixed()
     {
+        return $this->wholeNumbers > 0;
     }
 
     public function reduce()
     {
+        $r = $this->reduceFraction($this->numerator, $this->denominator);
+        $this->numerator = $r[0];
+        $this->denominator = $r[1];
+        return $this;
     }
 
     public function add()
